cea553079a2a6a022168895b3e73baf5a71900db
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-07-22 11:52:18 +0200

    Add logger

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const main = () => {
   try {
     Args_client.from_args(process.argv.slice(2))
       .then(Cli_client.client)
-      .catch((e) => console.error(e));
+      .catch((e) => logger.error(e));
   } catch (e) {
     logger.error(e);
     process.exit(1);
