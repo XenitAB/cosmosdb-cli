273b257ad206ec737d3cf40ffdc4fe6c0eca76a8
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-07-23 00:09:36 +0200

    US is king 2

--- src/clients/cli.ts
+++ src/clients/cli.ts
@@ -5,55 +5,53 @@ import * as Cosmosdb_client from "./cosmosdb";
 import * as Storageaccount_client from "./storageaccount";
 import * as Backup_client from "./backup";
 
-const pretty_print_enum = <T>(e: T) => Object.values(e).join(", ");
-
 export const client = (args: Commands_model.commands): Promise<void> => {
   switch (args.command) {
     case Commands_model.Command.backup:
       if (args.sub_command) {
         switch (args.sub_command) {
-          case Commands_model.Backup_sub_command.azure_storage_account:
-            return Config_client.to_config({
+          case Commands_model.Backup_sub_command.azure_storage_account: {
+            const config = Config_client.to_config({
               ...Config_model.cosmosdb,
               ...Config_model.azure_storage_account,
-            }).then((config) =>
-              Cosmosdb_client.client(
-                config.get("cosmosdb_account_endpoint"),
-                config.get("cosmosdb_account_key")
-              )
-                .then(Cosmosdb_client.get_all_items)
-                .then((items_by_containers) =>
-                  Storageaccount_client.container_client(
-                    config.get("storage_account_name"),
-                    config.get("storage_account_container"),
-                    config.get("storage_account_key")
-                  ).then((container_client) =>
-                    Backup_client.backup_cosmosdb_containers_to_storage_account_blob(
-                      container_client,
-                      items_by_containers,
-                      `${Date.now().toString()}/`
-                    )
-                  )
-                )
-            );
-          case Commands_model.Backup_sub_command.filesystem:
-            return Config_client.to_config({
-              ...Config_model.cosmosdb,
-              ...Config_model.filesystem,
-            }).then((config) =>
-              Cosmosdb_client.client(
-                config.get("cosmosdb_account_endpoint"),
-                config.get("cosmosdb_account_key")
-              )
-                .then(Cosmosdb_client.get_all_items)
-                .then((items_by_containers) =>
-                  Backup_client.backup_cosmosdb_containers_to_filesystem(
+            });
+            return Cosmosdb_client.client(
+              config.get("cosmosdb_account_endpoint"),
+              config.get("cosmosdb_account_key")
+            )
+              .then(Cosmosdb_client.get_all_items)
+              .then((items_by_containers) =>
+                Storageaccount_client.container_client(
+                  config.get("storage_account_name"),
+                  config.get("storage_account_container"),
+                  config.get("storage_account_key")
+                ).then((container_client) =>
+                  Backup_client.backup_cosmosdb_containers_to_storage_account_blob(
+                    container_client,
                     items_by_containers,
-                    config.get("filesystem_path"),
                     `${Date.now().toString()}/`
                   )
                 )
-            );
+              );
+          }
+          case Commands_model.Backup_sub_command.filesystem: {
+            const config = Config_client.to_config({
+              ...Config_model.cosmosdb,
+              ...Config_model.filesystem,
+            });
+            return Cosmosdb_client.client(
+              config.get("cosmosdb_account_endpoint"),
+              config.get("cosmosdb_account_key")
+            )
+              .then(Cosmosdb_client.get_all_items)
+              .then((items_by_containers) =>
+                Backup_client.backup_cosmosdb_containers_to_filesystem(
+                  items_by_containers,
+                  config.get("filesystem_path"),
+                  `${Date.now().toString()}/`
+                )
+              );
+          }
         }
       } else {
         throw new Error("No sub_command defined.");
--- src/clients/config.ts
+++ src/clients/config.ts
@@ -1,13 +1,5 @@
 import Convict from "convict";
 
-export const to_config = <T>(
-  schema: Convict.Schema<T>
-): Promise<Convict.Config<T>> => {
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(Convict(schema));
-    } catch (e) {
-      reject(e);
-    }
-  });
+export const to_config = <T>(schema: Convict.Schema<T>): Convict.Config<T> => {
+  return Convict(schema);
 };
