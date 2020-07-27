92b08a07feba2cf75066f56678bbea7b0815c67e
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-07-27 23:26:45 +0200

    Remove debug log

--- src/clients/backup.ts
+++ src/clients/backup.ts
@@ -11,9 +11,6 @@ const save_item_to_storage_account = (
   suffix_string: string,
   delimiter_string: string
 ): Promise<void> => {
-  console.log("--------------");
-  console.log(items_by_containers);
-  console.log("--------------");
   const db_id = items_by_containers.db_id;
   const continer_id = items_by_containers.container_id;
   const blob_name = `${prefix_string}${db_id}${delimiter_string}${continer_id}${suffix_string}`;
