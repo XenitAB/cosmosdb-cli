b288a0dc48e7b812b25221200451ffd6bcb58463
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-08-01 20:18:08 +0200

    Remove export of main

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,10 @@ import * as Commands_model from "./models/commands";
 import * as Cli_client from "./clients/cli";
 import logger from "./clients/logger";
 
-export const main = (): Promise<void> => {
-  return Args_model.from_args(process.argv.slice(2))
-    .then(Commands_model.from_args)
-    .then(Cli_client.client)
-    .catch((e: Error) => {
-      logger.error(e);
-      process.exit(1);
-    });
-};
-
-main();
+Args_model.from_args(process.argv.slice(2))
+  .then(Commands_model.from_args)
+  .then(Cli_client.client)
+  .catch((e) => {
+    logger.error(e);
+    throw new Error(e);
+  });
