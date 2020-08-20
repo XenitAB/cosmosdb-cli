252fffd6776f748ae7a75d01bbcad2cf2e073f5f
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-08-20 17:37:27 +0200

    Cleanup branch

--- jest.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  projects: ["<rootDir>/jest-unit.config.js", "<rootDir>/jest-int.config.js"],
-};
--- src/clients/cli/index.ts
+++
@@ -1,5 +0,0 @@
-import * as Commands_model from "../../models/commands";
-
-export const client = (args: Commands_model.commands): Promise<void> => {
-  return import(`./${args.command}`).then((command) => command.client(args));
-};
